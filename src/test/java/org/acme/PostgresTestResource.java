27d21c8be8a742aacdb00b90d5ed357bd202c375
sapph <sapph1978@gmail.com>
2026-02-18 13:10:18 +0100

    Added postgresql to testcontainers

---
+++ src/test/java/org/acme/PostgresTestResource.java
@@ -0,0 +1,32 @@
+package org.acme;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PostgresTestResource implements QuarkusTestResourceLifecycleManager {
+    private PostgreSQLContainer<?> postgres;
+
+    @Override
+    public Map<String, String> start() {
+        postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+        postgres.start();
+
+        Map<String, String> props = new HashMap<>();
+        props.put("quarkus.datasource.db-kind", "postgresql");
+        props.put("quarkus.datasource.jdbc.url", postgres.getJdbcUrl());
+        props.put("quarkus.datasource.username", postgres.getUsername());
+        props.put("quarkus.datasource.password", postgres.getPassword());
+        return props;
+    }
+
+    @Override
+    public void stop() {
+        if (postgres != null) {
+            postgres.stop();
+        }
+    }
+}
+
